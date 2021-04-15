eaaa5d3566bb734a01a436cd8c74f451f3ceaefe
Pedro Oliveira Silva <psilva@dunelm.com>
2021-04-15 19:55:57 +0100

    Export all types

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,13 @@
 export * from './api/riotValorantApi';
+export * from './api/valorant/ranked/rankedV1';
+export * from './api/valorant/status/statusV1';
+
+export * from './types/valorant/regions';
+
+export * from './types/valorant/status/v1/act';
+export * from './types/valorant/status/v1/content';
+export * from './types/valorant/status/v1/contentItem';
+export * from './types/valorant/status/v1/localizedNames';
+
+export * from './types/valorant/ranked/v1/player';
+export * from './types/valorant/ranked/v1/leaderboard';
